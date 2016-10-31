62dd83b4d898814bc885535b683e74927aa83920
R. Harish Navnit <harishnavnit@gmail.com>
2016-10-31 11:44:31 +0530

    Do directory/file checks in script.

--- mount.sh
+++ mount.sh
@@ -1,2 +1,17 @@
-sudo mount /dev/sda2 /media/harish/regularStorage
-sudo mount /dev/sdb3 /media/harish/reserveStorage
+# Mount other partitions
+if [ -d "/media/harish/regularStorage/home" ]
+     # home directory exists
+then # do nothing
+else
+    sudo mount /dev/sda2 /media/harish/regularStorage
+    sudo mount /dev/sdb3 /media/harish/reserveStorage
+fi
+
+# Create pid file in cache for cantata profiles.
+if [ -e "/home/harish/.cache/cantata/mpd/pid" ]
+    # pid file present in cache
+then # do nothing
+else
+    mkdir ~/.cache/cantata/mpd/
+    touch ~/.cache/cantata/mpd/pid
+fi
