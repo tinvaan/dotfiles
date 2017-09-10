2f00b1e7328ce6e528aea164ea38c233805da9d9
R. Harish Navnit <harishnavnit@gmail.com>
2017-09-11 02:01:33 +0530

    Fix mount script for the XPS.

--- mount-xps.sh
+++ mount-xps.sh
@@ -3,6 +3,9 @@ if [ -d "/run/media/harish/WORK/src" ]
 	# src directory exists
 then # do nothing
 else
+	sudo mkdir /run/media
+	sudo mkdir /run/media/harish
+	sudo mkdir /run/media/harish/WORK
+
 	sudo mount /dev/nvme0n1p4 /run/media/harish/WORK
 fi
-
